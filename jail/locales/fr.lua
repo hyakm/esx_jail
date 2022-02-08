fb9ff9bf1e7fce21cf023ca8aad5e9a01b241e17
hyakm <97416241+hyakm@users.noreply.github.com>
2022-02-08 20:08:18 +0100

    Add files via upload

---
+++ jail/locales/fr.lua
@@ -0,0 +1,8 @@
+Locales ['fr'] = {
+	['blip_name']          = 'Prison',
+	['judge']              = 'Juge',
+	['escape_attempt']     = 'vous n’avez pas le droit d’échapper à la prison!',
+	['remaining_msg']      = 'il reste ~b~%s~s~ secondes jusqu’à ce que vous êtes libéré de prison',
+	['jailed_msg']         = '%s est maintenant en prison pour %s minutes',
+	['unjailed']           = '%s a été libéré de prison!'
+}
